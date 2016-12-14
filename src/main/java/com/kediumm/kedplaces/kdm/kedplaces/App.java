335591d467afb733adbf9ba9d7ab6f61e166705c
Miguel Vale Teixeira <miguel@kediumm.com>
2016-12-14 12:45:38 +0000

    create project kedPlaces
    Signed-off-by: Miguel Vale Teixeira <miguel@kediumm.com>

---
+++ src/main/java/com/kediumm/kedplaces/kdm/kedplaces/App.java
@@ -0,0 +1,13 @@
+package com.kediumm.kedplaces.kdm.kedplaces;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
